c51d83984344583ee451fafd0599cef19b25c3f2
nguyentruongvttk2003 <152682756+nguyentruongvttk2003@users.noreply.github.com>
2024-10-23 23:51:35 +0700

    done

--- Ex01/app/src/main/java/com/example/ex01/MainActivity.java
+++ Ex01/app/src/main/java/com/example/ex01/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtA, edtB, edtKQ;
+    private Button btncong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+// Ánh xạ Id cho các biến giao diện
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btncong = findViewById(R.id.btntong);
+// Xử lý tương tác với người dùng
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(edtA.getText().toString());
+                //Lấy dữ liệu từ edtA, ép sang kiểu int và gán vào biến a
+                int b = Integer.parseInt(edtB.getText().toString());
+                int c = a + b;
+                edtKQ.setText(c + ""); //Hiển thị kết quả
+            }
         });
     }
 }
