f195d803c2fda52f1dbf3e262eec78b3eea850de
nguyentruongvttk2003 <152682756+nguyentruongvttk2003@users.noreply.github.com>
2024-10-26 09:43:48 +0700

    done

--- Ex09/app/src/main/java/com/example/ex09/MainActivity.java
+++ Ex09/app/src/main/java/com/example/ex09/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex09;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btn_play, btn_stop;
+    Boolean flag = true;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn_play = findViewById(R.id.btn_play);
+        btn_stop = findViewById(R.id.btn_stop);
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, Myservice.class);
+                startService(intent1);
+                if (flag == true) {
+                    btn_play.setImageResource(R.drawable.stop);
+                    flag = false;
+                } else {
+                    btn_play.setImageResource(R.drawable.play);
+                    flag = true;
+                }
+            }
+        });
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity.this, Myservice.class);
+                stopService(intent2);
+                btn_play.setImageResource(R.drawable.play);
+            }
         });
     }
 }
